8516f6ca7ff604e8229e97760b33d525931c7f22
shriyans <shriyans.n@somaiya.edu>
2022-03-15 17:53:26 +0530

    folder structuring

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+    SafeAreaView,
+    ScrollView,
+    StatusBar,
+    StyleSheet,
+    Text,
+    useColorScheme,
+    View,
+  } from 'react-native';
+
+
+const App = () => {
+    return (
+      <View>
+          <Text>Hello</Text>
+      </View>
+    );
+}
+
+export default App;
